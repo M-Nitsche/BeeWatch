fc927e3b3a9012db8e68e30d05097fc36897a255
M-Nitsche <43791419+M-Nitsche@users.noreply.github.com>
2021-07-06 13:55:38 +0200

    Added noise reduction

--- tracking/background_subtractor.py
+++ tracking/background_subtractor.py
@@ -1,15 +1,17 @@
 import cv2 as cv
 
-cap = cv.VideoCapture("video_data/bees_demo.mp4")
+cap = cv.VideoCapture("../dataset/example_video.mp4")
 #cap = cv.VideoCapture("video_data/bee_video_short.mp4")
 
 
 fgbg = cv.createBackgroundSubtractorMOG2()
+kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(2,2))
 
 while(1):
     ret, frame = cap.read()
 
     fgmask = fgbg.apply(frame)
+    fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
 
     cv.imshow('frame',fgmask)
     k = cv.waitKey(30) & 0xff
